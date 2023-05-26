dfb01adc94710fc2cdfbe7dc17196cd7f6f170c0
hamizan024 <hamizanfr024@gmail.com>
2023-05-26 19:46:50 +0700

    Add Icons

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,4 +1,6 @@
 import React, { useRef } from "react";
+import { AiFillEdit } from "react-icons/ai";
+import { IoCheckmarkDoneSharp, IoClose } from "react-icons/io5";
 
 const TodoItem = (props) => {
   const { item, updateTodo, removeTodo, CompleteTodo } = props;
@@ -28,9 +30,21 @@ const TodoItem = (props) => {
         onKeyPress={(e) => update(item.id, inputRef.current.value, e)}
       />
       <div className="btns">
-        <button onClick={() => changeFocus()}>Edit</button>
-        <button onClick={() => CompleteTodo(item.id)}>Complete</button>
-        <button onClick={() => removeTodo(item.id)}>Delete</button>{" "}
+        <button onClick={() => changeFocus()}>
+          {" "}
+          <AiFillEdit />{" "}
+        </button>
+        <button 
+        style={{color:"green"}}
+        onClick={() => CompleteTodo(item.id)}>
+          <IoCheckmarkDoneSharp />
+        </button>
+        <button 
+        style={{color:"red"}}
+        onClick={() => removeTodo(item.id)}>
+          {" "}
+          <IoClose />
+        </button>{" "}
       </div>
       {item.completed && <span className="completed">done</span>}
     </li>
